861e9f80332314a94cf5ab7e46f511d1ebe4deaf
ardianugerahw <ardianugerahw@gmail.com>
2024-06-16 18:44:01 +0700

    mengganti fitur detail
    Terdapat perubahan plan dari fitur detail menjadi fitur alternatif modal

--- web-app/src/scripts/views/app.js
+++ web-app/src/scripts/views/app.js
@@ -1,4 +1,6 @@
 import DrawerInitiator from '../utils/drawer-initiator';
+import routes from '../routes/routes';
+import UrlParser from '../routes/url-parser';
 
 class App {
   constructor({ button, drawer, content }) {
@@ -20,7 +22,27 @@ class App {
   }
 
   async renderPage() {
-    // tes render
+    const url = UrlParser.parseActiveUrlWithCombiner();
+    let page;
+
+    try {
+      page = await routes[url]; // Assuming routes is a Promise or async function
+    } catch (error) {
+      console.error('Error fetching route:', error);
+      // Handle error gracefully (e.g., display an error message)
+    }
+
+    if (page) {
+      this._content.innerHTML = await page.render();
+      await page.afterRender();
+      const skipLinkElem = document.querySelector('.skip-link');
+      skipLinkElem.addEventListener('click', (event) => {
+        event.preventDefault();
+        document.querySelector('#maincontent').focus();
+      });
+    } else {
+      // Handle missing route (e.g., display a "Not Found" message)
+    }
   }
 }
 
